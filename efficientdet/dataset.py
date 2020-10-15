9f942ac2b5df9b818ded4c9615a4bca726a95b32
root <you@example.com>
2020-10-15 09:06:41 +0000

    mod

--- efficientdet/dataset.py
+++ efficientdet/dataset.py
@@ -120,17 +120,22 @@ def collater(data):
 
 class Coustom_augment(object):
     def __init__(self):
+        sometimes = lambda aug: iaa.Sometimes(0.5, aug)
         self.seq = iaa.Sequential([
-                          #iaa.Rot90([1,2,3,4]),
-                          #iaa.Rotate((-90,90)),
-                          iaa.Fliplr(0.5),
-                          iaa.Flipud(0.5),
-                          iaa.Affine(scale=(0.5, 1.5), rotate=(-380, 380), translate_percent={'x': (-0.2, 0.2), 'y': (-0.2, 0.2)}, mode=['symmetric', 'reflect'], cval=(0,255)),
-                          #iaa.GammaContrast((0, 2.0)),
-                          #iaa.MultiplyHueAndSaturation((0.5, 1.5), per_channel=True),
-                          iaa.MultiplyAndAddToBrightness(mul=(0.5, 1.5), add=(-10, 10)),   
-                          #iaa.CLAHE()
-                                 ])
+                        iaa.Fliplr(0.5),
+                        iaa.Flipud(0.5),
+                        iaa.Affine(scale=(0.5, 1.5), rotate=(-380, 380),
+                                translate_percent={'x': (-0.2, 0.2), 'y': (-0.2, 0.2)},
+                                mode=['symmetric', 'reflect'], cval=(0, 255)),
+                        iaa.GammaContrast((0, 2.0)),
+                        sometimes(iaa.SomeOf(1, [iaa.MultiplyAndAddToBrightness(mul=(0.5,
+                                1.5), add=(-10, 10)), iaa.Grayscale(alpha=(0.0, 1.0))])),
+                        sometimes(iaa.SomeOf(1, [iaa.GaussianBlur(sigma=(0.2, 2.0)),
+                                iaa.AverageBlur(k=(1, 5)), iaa.MedianBlur(k=(1, 5)),
+                                iaa.BilateralBlur(d=(3, 10), sigma_color=(10, 250),
+                                sigma_space=(10, 250))])),
+                        sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.15))),
+                    ])
 
     def __call__(self, sample):
         image, annots = sample['img'], sample['annot']
